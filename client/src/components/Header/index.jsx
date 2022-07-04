3253b23cd71936f685e178bbad3c489138cf5843
sary <arch.selmelegy@gmail.com>
2022-07-04 16:05:49 +0200

    Header user info area

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -16,7 +16,17 @@ const index = () => {
         <div className="Header-userArea-notifications">
           <i class="fa-solid fa-bell"></i>
         </div>
-        <div className="Header-userArea-info"></div>
+        <div className="Header-userArea-info">
+          <div className="Header-userArea-info-pp">
+            <img
+              src="/images/user1.jpg"
+              alt="profile-pic"
+              className="Header-userArea-info-ppImg"
+            />
+          </div>
+          <p className="Header-userArea-info-name">Harry Wilson</p>
+          <i class="Header-userArea-info-icon fa-solid fa-angle-down"></i>
+        </div>
       </div>
     </div>
   );
