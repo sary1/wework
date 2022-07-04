5273784d5dee1f952531f47b68e307b40f900393
sary <arch.selmelegy@gmail.com>
2022-07-04 16:53:50 +0200

    Profile body title and organization

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 
+import "./Profile.scss";
+
 const Profile = () => {
   return (
     <div className="Profile">
       <Navbar />
-      <div className="body">
+      <div className="Profile-body">
         <Header />
+        <h1 className="Profile-body-title">My Profile</h1>
+        <div className="Profile-body-content">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet ab
+          earum provident error distinctio natus inventore facere quia, alias
+          corporis dolores reprehenderit deleniti magni. Doloribus nulla
+          aspernatur quod omnis voluptatem aut suscipit minima eius, ea
+          perferendis culpa, obcaecati vel temporibus recusandae. Doloribus
+          aliquam sapiente vitae odio itaque libero, laborum provident qui dicta
+          ducimus at nam? Non perspiciatis dolorem ab praesentium laudantium
+          harum nihil eius iusto omnis tempore! Fuga, maxime. Iure sapiente
+          harum iusto quod ipsam dignissimos eius optio. Repudiandae magni rerum
+          deleniti, tempore obcaecati sed dolores ab necessitatibus! Excepturi
+          ducimus quidem officia quod aliquam, dignissimos natus beatae
+          temporibus tempore adipisci!
+        </div>
       </div>
     </div>
   );
