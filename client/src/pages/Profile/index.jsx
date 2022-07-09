591a4061d06e3e61055dae3a0ffad4d4dba9f6e3
sary <arch.selmelegy@gmail.com>
2022-07-09 20:49:08 +0200

    Profile page body initial structure

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -4,7 +4,15 @@ import Navbar from "../../components/Navbar";
 import "./Profile.scss";
 
 const Profile = () => {
-  return <div className="Profile"></div>;
+  return (
+    <div className="Profile">
+      <h1 className="Profile-header">My Profile</h1>
+      <div className="Profile-body">
+        <div className="Profile-body-info"></div>
+        <div className="Profile-body-dashboard"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
